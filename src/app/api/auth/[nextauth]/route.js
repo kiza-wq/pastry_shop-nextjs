708072ad11303723958be1e9b9f0500e64e93287
kiza-wq <kiza152@gmail.com>
2026-05-02 17:55:08 +0200

    Add files via upload

---
+++ src/app/api/auth/[nextauth]/route.js
@@ -0,0 +1,70 @@
+import NextAuth, { getServerSession } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import * as mongoose from "mongoose";
+import { User } from "@/app/models/User";
+import bcrypt from "bcrypt";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/libs/mongoConnect";
+import { UserInfo } from "@/app/models/UserInfo";
+
+export const authOptions = {
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+        const password0k = user && bcrypt.compareSync(password, user.password);
+
+        if (password0k) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt", // Credentials provider requires JWT sessions
+  },
+};
+
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({ email: userEmail });
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
+}
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+
+//for production: https://{YOUR_DOMAIN}/api/auth/callback/google
+//for development: http://localhost:3000/api/auth/callback/google
