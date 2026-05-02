ea8b936cf55e5e6d419cb48eb8e897cda5678b65
kiza-wq <kiza152@gmail.com>
2026-05-02 16:46:59 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pastry-shop-wq9698.s3.amazonaws.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
